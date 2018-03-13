7324616f0220b506ad42a312a0e264060ce74582
allen4dev <alanaliagadev@gmail.com>
2018-03-13 00:15:38 -0400

    [Add] 1_basics: Comment migration and model factory

--- 1_basics/database/factories/PostFactory.php
+++ 1_basics/database/factories/PostFactory.php
@@ -7,6 +7,9 @@ $factory->define(App\Post::class, function (Faker $faker) {
         'user_id' => function() {
             return factory(App\User::class)->create()->id;
         },
+        'category_id' => function() {
+            return factory(App\Category::class)->create()->id;
+        },
         'title' => $faker->sentence,
         'body' => $faker->paragraph
     ];
