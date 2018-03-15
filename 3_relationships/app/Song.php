cc983be79620bb5444635dda30b8573152b7faf2
allen4dev <alanaliagadev@gmail.com>
2018-03-15 15:29:29 -0400

    [Add] 3_relationships: One to Many (Category - Song, Artist - Song)

--- 3_relationships/app/Song.php
+++ 3_relationships/app/Song.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Song extends Model
 {
-    //
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function artist()
+    {
+        return $this->belongsTo(Artist::class);
+    }
 }
