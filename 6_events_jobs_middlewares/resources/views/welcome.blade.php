fb2a3d788858e50ce93ce5319cbb8472559292d5
allen4dev <alanaliagadev@gmail.com>
2018-03-28 00:47:49 -0400

    [Add] 6_events_jobs_middlewares: basic middleware

--- 6_events_jobs_middlewares/resources/views/welcome.blade.php
+++ 6_events_jobs_middlewares/resources/views/welcome.blade.php
@@ -66,18 +66,11 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
             <div class="content">
+                @if (Session::has('message'))
+                    <p>{{Session::get('message')}}</p>
+                @endif
+
                 <div class="title m-b-md">
                     Laravel
                 </div>
--- 6_events_jobs_middlewares/routes/web.php
+++ 6_events_jobs_middlewares/routes/web.php
@@ -10,15 +10,36 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
+    return view('welcome');
+    // event(new \App\Events\UserWasBanned($user));
+})->name('home');
+
+Route::get('/login', function () {
     DB::table('users')->truncate();
 
     $user = \App\User::forceCreate([
         'name' => 'Allen',
         'email' => 'allen@example.test',
         'password' => bcrypt('supersecret'),
+        'subscribed' => true,
     ]);
 
-    event(new \App\Events\UserWasBanned($user));
-});
+    auth()->login($user);
+
+    return redirect('/dashboard');
+})->name('login');
+
+Route::get('/logout', function () {
+    auth()->logout();
+
+    return redirect('/');
+})->name('logout');
+
+Route::get('/dashboard', ['middleware' => ['auth'], function () {
+    dd('This page can be viewed by logged in users.');
+}])->name('dashboard');
+
+Route::get('/courses', ['middleware' => ['auth', 'subscribed'], function () {
+    dd('This page can be viewed by logged in users, but also need to be subscribed.');
+}])->name('courses');
